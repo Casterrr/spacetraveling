f31b03eabf13fa30e022a8a489bd44a484060fb5
Lucas <lucasmvl2003@gmail.com>
2022-10-24 10:14:09 -0300

    temporary completed

---
+++ README.md
@@ -0,0 +1,61 @@
+<h1 align="center">
+    <img src="https://github.com/rodrigopfialho/spacetraveling/blob/master/public/images/Logo.svg">
+</h1>
+
+## SpaceTraveling | Next.js
+
+<p>
+    Aplicação desenvolvida com base nos desafios proposto pela Rocketseat
+</p>
+
+## Sobre 
+
+<p>
+    SpaceTraveling é um desafio proposto na trilha do ignite, com base em um template fornecido
+    pela equipe da Rocketseat. O desafio é a construção de um blog, com o gerenciador de conteúdo 
+    Prismic CMS e utilizando conceitos do nextJS como o  de Static Site Generation (SSG).
+    <hr>
+    Foi adicionado ao projeto sobre o pedido do deafio complementar alguns desafios como o Preview, 
+    verificação se o poste foi editado e a adição de comentários utilizando o Github.
+
+</p>
+
+Link do [desafio](https://www.notion.so/Desafio-01-Criando-um-projeto-do-zero-b1a3645d286b4eec93f5f1f5476d0ff7)
+
+Link desafio [complementar](https://www.notion.so/Desafio-02-Adicionando-features-ao-blog-d466866c02544c79bbada9717c033d0a)
+
+## Tecnologias
+
+<span>O projeto foi contruído com as seguintes tecnologias</span>
+
+- [ReactJS](https://reactjs.org/)
+- [NextJS](https://nextjs.org/)
+- [Prismic CMS](https://prismic.io/)
+- [Typescript](https://www.typescriptlang.org/)
+- [Utterances](https://utteranc.es/)
+
+## Configurações do projeto
+
+- Realizar o clone do projeto 
+```
+git clone https://github.com/rodrigopfialho/spacetraveling.git
+```
+- Realizar as instalações das dependências 
+```
+yarn 
+
+- Inicializar o Projeto
+
+yarn dev
+```
+<span>O projeto estará disponivel no  http://localhost:3000</span>
+
+## Screenshots
+![App Screenshot](https://github.com/rodrigopfialho/spacetraveling/blob/master/public/images/p1.png)
+![App Screenshot](https://github.com/rodrigopfialho/spacetraveling/blob/master/public/images/p2.png)
+
+## License
+
+[![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://github.com/tterb/atomic-design-ui/blob/master/LICENSEs)
+
+<span>Feito com 💜 por [Rodrigo](https://www.linkedin.com/in/rodrigo-pereira-2635391b2/)</span>
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,12 +1,23 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { useRouter } from 'next/router';
+import Head from 'next/head';
 
+import { FiCalendar, FiUser, FiClock } from 'react-icons/fi';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { RichText } from 'prismic-dom';
+import Prismic from '@prismicio/client';
 import { getPrismicClient } from '../../services/prismic';
-
+import Link from 'next/link'
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
+import Comments from '../../components/Comments';
+import { useMemo } from 'react';
 
 interface Post {
   first_publication_date: string | null;
+  last_publication_date: string | null;
+  uid: string;
   data: {
     title: string;
     banner: {
@@ -23,23 +34,185 @@ interface Post {
 }
 
 interface PostProps {
+  nextPost: Post | null;
+  prevPost: Post | null;
+  preview: boolean;
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post({ post, preview, prevPost, nextPost }: PostProps): JSX.Element {
+  const router = useRouter();
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient({});
-//   const posts = await prismic.getByType(TODO);
+  const editPost = useMemo(() => {
+    if (router.isFallback) {
+      return false
+    }
 
-//   // TODO
-// };
+    return post.last_publication_date !== post.first_publication_date;
+  }, [router.isFallback])
 
-// export const getStaticProps = async ({params }) => {
-//   const prismic = getPrismicClient({});
-//   const response = await prismic.getByUID(TODO);
+  const totalWords = post.data.content.reduce((sumWords, content) => {
+    const contentText = RichText.asText(content.body);
+    const arrayWords = contentText.split(' ').filter(text => text !== '');
 
-//   // TODO
-// };
+    return sumWords + arrayWords.length;
+  }, 0);
+
+  if (router.isFallback) {
+    return <div className={commonStyles.container}>Carregando...</div>;
+  }
+
+  return (
+    <>
+      <Head>
+        <title>{post.data.title} | spacetraveling</title>
+      </Head>
+
+      <img src={post.data.banner.url} alt="Banner" className={styles.banner} />
+      <div className={commonStyles.container}>
+        <h2 className={`${commonStyles.heading} ${styles.title}`}>
+          {post.data.title}
+        </h2>
+        <div className={styles.details}>
+          <span>
+            <FiCalendar />
+            {format(new Date(post.first_publication_date), 'd MMM yyyy', {
+              locale: ptBR,
+            })}
+          </span>
+          <span>
+            <FiUser />
+            {post.data.author}
+          </span>
+          <span>
+            <FiClock />
+            {`${Math.ceil(totalWords / 200)} min`}
+          </span>
+
+          {/* {editPost && (
+            <p>
+              * editado em
+              {format(new Date(post.last_publication_date), "d MMM yyyy', às 'HH:mm'", {
+                locale: ptBR
+              })}
+            </p>
+          )} */}
+
+        </div>
+
+        {post.data.content.map(content => (
+          <div key={content.heading} className={styles.content}>
+            <h3>{content.heading}</h3>
+            <div
+              // eslint-disable-next-line react/no-danger
+              dangerouslySetInnerHTML={{
+                __html: RichText.asHtml(content.body),
+              }}
+            />
+          </div>
+        ))}
+
+        <footer className={styles.prevNext}>
+          <div>
+            {prevPost && (
+              <Link href={`/post/${prevPost.uid}`}>
+                <a>
+                  <h2>{prevPost.data.title}</h2>
+                  <span>Post anterior</span>
+                </a>
+              </Link>
+            )}
+          </div>
+          <div className={styles.next}>
+             { nextPost && (
+                <Link href={`/post/${nextPost.uid}`}>
+                <a>
+                  <h2>{nextPost.data.title}</h2>
+                  <span>Próximo post</span>
+                </a>
+              </Link>
+             )}
+          </div>
+          
+        </footer>
+        <Comments />
+        
+          <aside className={commonStyles.exitPreview}>
+            <Link href="/api/exit-preview">
+              <a>Sair do modo Preview</a>
+            </Link>
+          </aside>
+      
+      </div>
+      
+    </>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const prismic = getPrismicClient();
+  const posts = await prismic.query(
+    [Prismic.predicates.at('document.type', 'posts')],
+    {
+      fetch: ['posts.title', 'posts.subtitle', 'posts.author'],
+      pageSize: 2,
+    }
+  );
+
+  const paths = posts.results.map(post => ({
+    params: { slug: post.uid },
+  }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({
+  params,
+  preview = false,
+  previewData
+}) => {
+  const { slug } = params;
+
+  const prismic = getPrismicClient();
+  const response = await prismic.getByUID('posts', String(slug), {
+    ref: previewData?.ref ?? null,
+  });
+
+  const post = {
+    uid: response.uid,
+    first_publication_date: response.first_publication_date,
+    last_publication_date: response.last_publication_date,
+    data: response.data,
+  };
+
+  
+  const prevPost = (
+    await prismic.query(Prismic.predicates.at('document.type', 'posts'), {
+      pageSize: 1,
+      after: response.id,
+      orderings: '[document.first_publication_date]',
+      fetch: ['posts.title'],
+    })
+  ).results[0];
+
+  const nextPost = (
+    await prismic.query(Prismic.predicates.at('document.type', 'posts'), {
+      pageSize: 1,
+      after: response.id,
+      orderings: '[document.first_publication_date desc]',
+      fetch: ['posts.title'],
+    })
+  ).results[0];
+
+  return {
+    props: {
+      post,
+      prevPost: prevPost ?? null,
+      nextPost: nextPost ?? null,
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  };
+};
